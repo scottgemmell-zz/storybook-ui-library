bd346f74fc2c467eb8f64ba569ad21d1107422d6
Scott Gemmell <scott.gemmell@mac.com>
2019-04-07 19:19:50 +0100

    Info addon

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,26 +4,32 @@ import { storiesOf } from '@storybook/react';
 import { withKnobs, select } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withInfo } from '@storybook/addon-info';
 
 import { Welcome } from '@storybook/react/demo';
 import { Button, ButtonGroup } from "../src/library/atoms";
 
-const options = {
-	Primary: 'primary',
-	Secondary: 'secondary',
-	Tertiary: 'tertiary'
-  };
+// const options = {
+// 	Primary: 'primary',
+// 	Secondary: 'secondary',
+// 	Tertiary: 'tertiary'
+// };
 
 storiesOf('Welcome', module)
 	.add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-	storiesOf('Button', module)
+storiesOf('Button', module)
 	.addDecorator(withKnobs)
+	.addDecorator(withInfo)
 	.add('Default', () => (
 		<Button onClick={action('clicked')}>Default</Button>
 	))
 	.add('Primary', () => (
-    	<Button onClick={action('clicked')} modifier={`${select("Modifier", options, "primary")}`}>Primary</Button>
+		<Button 
+			onClick={action('clicked')} 
+			modifier="primary" 
+			//modifier={`${select("Modifier", options, "primary")}`}
+		>Primary</Button>
 	));
 
 storiesOf('ButtonGroup', module)
